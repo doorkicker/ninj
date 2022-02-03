7bf47476455d1c224771739a9cb1fc9df24bfe8f
Wisecrack <wisecrackdoesathing@protonmail.com>
2022-02-03 03:19:52 -0500

    finish factorization code.

--- nsbase.py
+++ nsbase.py
@@ -67,6 +67,215 @@ def factor(var):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
+					31, 37, 41, 43, 47, 53, 59, 61, 67, 
+					71, 73, 79, 83, 89, 97, 101, 103, 
+					107, 109, 113, 127, 131, 137, 139, 
+					149, 151, 157, 163, 167, 173, 179, 
+					181, 191, 193, 197, 199, 211, 223, 
+					227, 229, 233, 239, 241, 251, 257, 
+					263, 269, 271, 277, 281, 283, 293, 
+					307, 311, 313, 317, 331, 337, 347, 349] 
+
+def nBitRandom(n): 
+	return rnd.randrange(Dec(2)**(n-1)+1, 2**n - 1) 
+
+def getLowLevelPrime(n): 
+	'''Generate a prime candidate divisible 
+	by first primes'''
+	while True: 
+		# Obtain a random number 
+		pc = nBitRandom(n) 
+
+		# Test divisibility by pre-generated 
+		# primes 
+		for divisor in first_primes_list: 
+			if pc % divisor == 0 and divisor**2 <= pc: 
+				break
+		else: return pc 
+
+
+
+
+
+
+
+
+# Utility function to do 
+# modular exponentiation. 
+# It returns (x^y) % p 
+def power(x, y, p): 
+	
+	# Initialize result 
+	res = 1; 
+	
+	# Update x if it is more than or 
+	# equal to p 
+	x = x % p; 
+	while (y > 0): 
+		
+		# If y is odd, multiply 
+		# x with result 
+		if (y & 1): 
+			res = (res * x) % p; 
+
+		# y must be even now 
+		y = y>>1; # y = y/2 
+		x = (x * x) % p; 
+	
+	return res; 
+
+
+
+
+# This function is called 
+# for all k trials. It returns 
+# false if n is composite and 
+# returns false if n is 
+# probably prime. d is an odd 
+# number such that d*2<sup>r</sup> = n-1 
+# for some r >= 1 
+def miillerTest(d, n): 
+	
+	# Pick a random number in [2..n-2] 
+	# Corner cases make sure that n > 4 
+	a = 2 + rnd.randint(1, n - 4); 
+
+	# Compute a^d % n 
+	x = power(a, d, n); 
+
+	if (x == 1 or x == n - 1): 
+		return True; 
+
+	# Keep squaring x while one 
+	# of the following doesn't 
+	# happen 
+	# (i) d does not reach n-1 
+	# (ii) (x^2) % n is not 1 
+	# (iii) (x^2) % n is not n-1 
+	while (d != n - 1): 
+		x = (x * x) % n; 
+		d *= 2; 
+
+		if (x == 1): 
+			return False; 
+		if (x == n - 1): 
+			return True; 
+
+	# Return composite 
+	return False; 
+
+
+
+# It returns false if n is 
+# composite and returns true if n 
+# is probably prime. k is an 
+# input parameter that determines 
+# accuracy level. Higher value of 
+# k indicates more accuracy. 
+def isPrime(n, k): 
+	
+	# Corner cases 
+	if (n <= 1 or n == 4): 
+		return False; 
+	if (n <= 3): 
+		return True; 
+
+	# Find r such that n = 
+	# 2^d * r + 1 for some r >= 1 
+	d = n - 1; 
+	while (d % 2 == 0): 
+		d //= 2; 
+
+	# Iterate given nber of 'k' times 
+	for i in range(k): 
+		if (miillerTest(d, n) == False): 
+			return False; 
+
+	return True; 
+
+
+
+
+def getPrime(bits):
+  i = 0
+  while isPrime(i, 40) == False:
+    i = getLowLevelPrime(bits)
+  
+  return i
+
+
+
+
+#GENERATE PRIMES
+def gen_factors(min_ratio=d('10000'), max_ratio=d('1000000')):
+  print("begin generating factors....")
+  dif = 0
+  a_low = 16 * mult
+  a_high = 32 * mult
+  b_low = 32 * mult
+  b_high = 48 * mult
+  while True:
+    print("generating...")
+    a = Dec(getPrime(Dec(rnd.randint(a_low, a_high))))
+    b = Dec(getPrime(Dec(rnd.randint(b_low, b_high))))
+    if a > b:
+      _ = a
+      a = b
+      b = _
+      
+    dif = b/a
+    print("done generating. ")
+    #if dif <= maxRatio and dif >= minRatio:
+    #  break
+    break
+    #if dif >= minRatio and dif <= maxRatio:
+    #  break
+  
+  return a, b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 '''
 #######################################
@@ -485,7 +694,11 @@ to find a factor by calling ns_factor
 #######################################
 '''
 def factor_prod():
-  p = d(input("enter a product to factor, leave blank to have one randomly generated for you: "))
+  p = ''
+  p = input("enter a product to factor, leave blank to have one randomly generated for you: ")
+  if p == '' or p == None:
+    a, b = gen_factors(d(1000), d(10000))
+    p = a*b
   result = ns_factor(p)
   
   pause()
